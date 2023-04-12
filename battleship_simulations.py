0f61a5d87edf5b3d1e6a1bf8fadb4b1318c94788
sraNUjhbOchTU0FXTuGa <67960183+sraNUjhbOchTU0FXTuGa@users.noreply.github.com>
2023-04-12 08:39:28 -0700

    Update battleship_simulations.py
    Added Weighted Probability AI

--- battleship_simulations.py
+++ battleship_simulations.py
@@ -175,6 +175,100 @@ class Battleship:
 
         return guess_row, guess_col
 
+
+    # improves standard probability map by weighting the smallest remaining ship as more important
+    def gen_weighted_prob_map(self):
+        prob_map = np.zeros([10, 10])
+        for ship_name in set(self.COORDINATE_SHIP_DICT.values()):
+            ship_size = self.SHIP_INFO[ship_name]
+            use_size = ship_size - 1
+
+
+            # weight hitting smallest remaining ship at 3x more valuable than every other ship
+            smallest_remaining_ship = min([self.SHIP_INFO[ship] for ship in self.COORDINATE_SHIP_DICT.values()])
+            weight = 1
+            if ship_size == smallest_remaining_ship:
+                if ship_size == 5:
+                    weight = 1
+                elif ship_size == 4:
+                    weight = 2
+                elif ship_size == 3:
+                    weight = 4
+                elif ship_size == 2:
+                    weight = 8
+
+            # check where a ship will fit on the board
+            for row in range(10):
+                for col in range(10):
+                    if self.SHOT_MAP[row][col] != 1:
+                        # get potential ship endpoints
+                        endpoints = []
+                        # add 1 to all endpoints to compensate for python indexing
+                        if row - use_size >= 0:
+                            endpoints.append(((row - use_size, col), (row + 1, col + 1)))
+                        if row + use_size <= 9:
+                            endpoints.append(((row, col), (row + use_size + 1, col + 1)))
+                        if col - use_size >= 0:
+                            endpoints.append(((row, col - use_size), (row + 1, col + 1)))
+                        if col + use_size <= 9:
+                            endpoints.append(((row, col), (row + 1, col + use_size + 1)))
+
+                        for (start_row, start_col), (end_row, end_col) in endpoints:
+                            if np.all(self.SHOT_MAP[start_row:end_row, start_col:end_col] == 0):
+                                prob_map[start_row:end_row, start_col:end_col] += weight
+
+                    # increase probability of attacking squares near successful hits
+                    if self.SHOT_MAP[row][col] == 1 and \
+                            self.SHIP_MAP[row][col] == 1 and \
+                            (row, col) not in self.SUNK_SHIP_COORDINATES:  # un-weight hits on sunk ships
+
+                        if (row + 1 <= 9) and (self.SHOT_MAP[row + 1][col] == 0):
+                            if (row - 1 >= 0) and \
+                                    (row - 1, col) not in self.SUNK_SHIP_COORDINATES and \
+                                    (self.SHOT_MAP[row - 1][col] == self.SHIP_MAP[row - 1][col] == 1):
+                                prob_map[row + 1][col] += 15
+                            else:
+                                prob_map[row + 1][col] += 10
+
+                        if (row - 1 >= 0) and (self.SHOT_MAP[row - 1][col] == 0):
+                            if (row + 1 <= 9) and \
+                                    (row + 1, col) not in self.SUNK_SHIP_COORDINATES and \
+                                    (self.SHOT_MAP[row + 1][col] == self.SHIP_MAP[row + 1][col] == 1):
+                                prob_map[row - 1][col] += 15
+                            else:
+                                prob_map[row - 1][col] += 10
+
+                        if (col + 1 <= 9) and (self.SHOT_MAP[row][col + 1] == 0):
+                            if (col - 1 >= 0) and \
+                                    (row, col - 1) not in self.SUNK_SHIP_COORDINATES and \
+                                    (self.SHOT_MAP[row][col - 1] == self.SHIP_MAP[row][col - 1] == 1):
+                                prob_map[row][col + 1] += 15
+                            else:
+                                prob_map[row][col + 1] += 10
+
+                        if (col - 1 >= 0) and (self.SHOT_MAP[row][col - 1] == 0):
+                            if (col + 1 <= 9) and \
+                                    (row, col + 1) not in self.SUNK_SHIP_COORDINATES and \
+                                    (self.SHOT_MAP[row][col + 1] == self.SHIP_MAP[row][col + 1] == 1):
+                                prob_map[row][col - 1] += 15
+                            else:
+                                prob_map[row][col - 1] += 10
+
+                    # decrease probability for misses to zero
+                    elif self.SHOT_MAP[row][col] == 1 and self.SHIP_MAP[row][col] != 1:
+                        prob_map[row][col] = 0
+
+        self.PROB_MAP = prob_map
+
+    def guess_weighted_prob(self):
+        self.gen_prob_map()
+        # get the row, col numbers of the largest element in PROB_MAP
+        # https://thispointer.com/find-max-value-its-index-in-numpy-array-numpy-amax/
+        max_indices = np.where(self.PROB_MAP == np.amax(self.PROB_MAP))
+        guess_row, guess_col = max_indices[0][0], max_indices[1][0]
+
+        return guess_row, guess_col
+
     def shoot(self, guess_row, guess_col):
         self.SHOT_MAP[guess_row][guess_col] = 1
         self.NUM_GUESSES += 1
@@ -194,7 +288,7 @@ class Battleship:
         start_time = time.time()
         all_guesses = []
         for i in range(num_games):
-            print(i) if i % 10 == 0 else 0
+            print(strategy, ":", i, "/", num_games) if i % 10 == 0 else 0
             self.place_ships()
             while not self.GAME_OVER:
                 if strategy == "random":
@@ -208,6 +302,8 @@ class Battleship:
                     guess_row, guess_col = self.hunt_target(smallest_remaining_ship)
                 elif strategy == "prob":
                     guess_row, guess_col = self.guess_prob()
+                elif strategy == "weighted_prob":
+                    guess_row, guess_col = self.guess_weighted_prob()
                 else:
                     raise Exception(f"invalid strategy chosen: {strategy}")
                 self.shoot(guess_row, guess_col)
@@ -217,33 +313,35 @@ class Battleship:
         return all_guesses
 
 
-def sim_all_strategies(num_games=1):
-    random_guesses = Battleship().simulate_games(num_games, strategy="random")
-    ht_guesses = Battleship().simulate_games(num_games, strategy="hunt_target")
-    ht_parity_guesses = Battleship().simulate_games(num_games, strategy="hunt_target_parity")
-    ht_parity_min_guesses = Battleship().simulate_games(num_games, strategy="hunt_target_min_parity")
-    prob_guesses = Battleship().simulate_games(num_games, strategy="prob")
+#def sim_all_strategies(num_games=1):
+#    random_guesses = Battleship().simulate_games(num_games, strategy="random")
+#    ht_guesses = Battleship().simulate_games(num_games, strategy="hunt_target")
+#    ht_parity_guesses = Battleship().simulate_games(num_games, strategy="hunt_target_parity")
+#    ht_parity_min_guesses = Battleship().simulate_games(num_games, strategy="hunt_target_min_parity")
+#    prob_guesses = Battleship().simulate_games(num_games, strategy="prob")
+#    prob_guesses = Battleship().simulate_games(num_games, strategy="weighted_prob")
 
-    return random_guesses, ht_guesses, ht_parity_guesses, ht_parity_min_guesses, prob_guesses
+#    return random_guesses, ht_guesses, ht_parity_guesses, ht_parity_min_guesses, prob_guesses
 
 
-def sim_strategy(file_num, num_games=200000):
-    prob_guesses = Battleship().simulate_games(num_games, strategy="hunt_target_min_parity")
-    FileStore = open(f"stored_objects/guesses_{file_num}.pickle", "wb")
+def sim_strategy(strategy, file_num, num_games=2000):
+    prob_guesses = Battleship().simulate_games(num_games, strategy)
+    FileStore = open(f"stored_objects/{strategy}.{file_num}.pickle", "wb")
     pickle.dump(prob_guesses, FileStore)
     FileStore.close()
 
 
 if __name__ == "__main__":
-    processes = []
-    for i in range(5):
-        p = multiprocessing.Process(target=sim_strategy, args=(str(i),))
-        p.start()
-        processes.append(p)
+    strategies = ["random", "hunt_target", "hunt_target_parity", "hunt_target_min_parity", "prob", "weighted_prob"]
+    num_threads = 6 #per strategy
+    num_games = 2000000
+    procs = []
+    for strategy in strategies:
+        print(strategy)
+        for i in range(num_threads):
+            p = multiprocessing.Process(target =sim_strategy, args=(strategy, i, int(num_games/num_threads)))
+            p.start()
+            procs.append(p)
 
-    for p in processes:
-        p.join()
-
-    all_guesses = []
-
-    print(len(all_guesses))
+    for p in procs:
+        p.join()    # wait for thread to finish
